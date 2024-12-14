d9ca1d59751089181830a6eb34af198ad765482c
devfemibadmus <devfemibadmus@gmail.com>
2024-12-14 10:03:56 +0100

    🚸 test 👷  pass ✨

--- test.py
+++ test.py
@@ -1,29 +1,20 @@
-import requests, os
-from ercaspay import load_env_vars
+from ercaspay import Ercaspay
 
-load_env_vars('.env')
+ercaspay = Ercaspay()
+response = ercaspay.initiate_Transaction(100.55, "test", "nigga@example.com", "nigga@example.com", redirectUrl='https://nigga.com')
+print(response)
 
-headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {os.environ.get('Authorization')}"
-}
-json = {
-    "amount": 10000,
-    "paymentReference": "johndoe@gmail.com",
-    "paymentMethods": "bank-transfer",
-    "customerName": "John Doe",
-    "customerEmail": "johndoe@gmail.com",
-    "customerPhoneNumber": "09061626364",
-    "redirectUrl": "https://omolabakeventures.com",
-    "description": "The description for this payment goes here",
-    "currency": "NGN",
-    "feeBearer": "customer",
-    "metadata": {
-        "firstname": "Ola",
-        "lastname": "Benson",
-        "email": "iie@mail.com"
-    }
-}
-response = requests.post('https://api.merchant.staging.ercaspay.com/api/v1/payment/initiate', headers=headers, json=json)
-print(response.json())
+transaction_ref = response['responseBody'].get('transactionReference')
+
+response = ercaspay.cancel_transaction(transaction_ref)
+print(response)
+response = ercaspay.details_transaction(transaction_ref)
+print(response)
+response = ercaspay.status_transaction('transaction_ref')
+print(response)
+"""
+response = ercaspay.details_transaction(transaction_ref)
+print(response)
+response = ercaspay.status_transaction(transaction_ref)
+print(response)
+"""
