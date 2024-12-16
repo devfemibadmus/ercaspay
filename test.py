366481b09b93d96eacc091136450a20a83f25411
devfemibadmus <devfemibadmus@gmail.com>
2024-12-16 01:10:27 +0100

    🔙 client test  🔧 pass :check: ➕

--- test.py
+++ test.py
@@ -1,20 +1,62 @@
 from ercaspay import Ercaspay
 
-ercaspay = Ercaspay()
-response = ercaspay.initiate_Transaction(100.55, "test", "nigga@example.com", "nigga@example.com", redirectUrl='https://nigga.com')
+
+transaction = Ercaspay()
+
+ref2 = 'ERCS|20241214192716|1734200836126'
+response = transaction.initiate(122200.55, "test", "nigga@example.com", "nigga@example.com", redirectUrl='https://nigga.com')
+print(response)
+response = transaction.status()
+print(response)
+response = transaction.details()
 print(response)
 
+card = {
+        'cvv' : '100',
+        'pin' : '1234',
+        'expiryDate' : '0139',
+        'pan' : '5123450000000008'
+    }
+browserDetails = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+    "3DSecureChallengeWindowSize": "FULL_SCREEN",
+    "acceptHeaders": "application/json",
+    "colorDepth": 24,
+    "javaEnabled": True,
+    "language": "en-US",
+    "screenHeight": 473,
+    "screenWidth": 1600,
+    "timeZone": 273
+}
+ipAddress = "41.242.77.212"
+response = transaction.card(card, browserDetails, ipAddress)
+print(response)
+
+"""
+response = transaction.initiate(100.55, "test", "nigga@example.com", "nigga@example.com", redirectUrl='https://nigga.com')
+print(response)
 transaction_ref = response['responseBody'].get('transactionReference')
 
-response = ercaspay.cancel_transaction(transaction_ref)
+response = transaction.bank_list()
 print(response)
-response = ercaspay.details_transaction(transaction_ref)
+response = transaction.bank(transaction_ref)
 print(response)
-response = ercaspay.status_transaction('transaction_ref')
+
+response = transaction.details('ERCS|20241214192716|1734200836126')
 print(response)
-"""
-response = ercaspay.details_transaction(transaction_ref)
+response = transaction.status('ERCS|20241214192716|1734200836126')
 print(response)
-response = ercaspay.status_transaction(transaction_ref)
+
+response = transaction.ussd('ERCS|20241214185857|1734199137582', 'fcmmb')
+print(response)
+response = transaction.cancel(transaction_ref)
+print(response)
+response = transaction.details(transaction_ref)
+print(response)
+response = transaction.status('transaction_ref')
+print(response)
+response = transaction.details(transaction_ref)
+print(response)
+response = transaction.status(transaction_ref)
 print(response)
 """
